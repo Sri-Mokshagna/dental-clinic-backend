15a0bbd815236840817eb5f0267673c626fef118
Jaladi Sri Mokshagna <109271984+Sri-Mokshagna@users.noreply.github.com>
2025-10-03 23:19:07 +0530

    Update WebConfig.java

--- src/main/java/com/dentalclinic/DentalClinic/config/WebConfig.java
+++ src/main/java/com/dentalclinic/DentalClinic/config/WebConfig.java
@@ -1,5 +1,16 @@
 package com.dentalclinic.DentalClinic.config;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
-
+    @Bean 
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://dental-clinic-frontend-1.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
